e89a1845eee757ff202d865989b2355543d2ac09
SemenShamaev <sema.shamaev1@gmail.com>
2020-02-06 20:20:32 +0900

    fdssdfsdfdsf

---
+++ Assets/Scripts/userController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(playerController))]
+public class userController : MonoBehaviour
+{
+    private playerController pc;
+    void Start()
+    {
+        pc = GetComponent<playerController>();
+    }
+
+  
+    void Update()
+    {
+        pc.Move(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+    }
+}
