7ccffff77667895ca9ae206e3cab10c2211e3c31
SemenShamaev <sema.shamaev1@gmail.com>
2020-04-04 23:39:39 +0900

    transition between levels

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomGenerator : MonoBehaviour
 {
     public enum TileType 
     {
-        Wall, Floor,
+        Wall, Floor, End, Start
     }
 
 	public int MapColumns; 
@@ -15,8 +16,10 @@ public class RoomGenerator : MonoBehaviour
     public GameObject[] floorTiles; 
     public GameObject[] wallTiles; 
     public GameObject[] containersTiles;
+    public GameObject[] endTiles;
 
     private Room[] rooms; 
+    private Room EndingRoom;
     private Corridor[] corridors; 
 
     public int roomWidth; 
@@ -25,18 +28,42 @@ public class RoomGenerator : MonoBehaviour
 
     public TileType[][] tiles;
     public GameObject Map; 
+    public GameObject Player;
     private List<Vector3> gridPositions = new List<Vector3>();
+    private Vector3 endPosition;
+
+    int level = 1;
+
+    void Update()
+    {
+        if(Player.transform.position == endPosition)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            ++level;
+        }
+    }
 
     private void Start()
+    {
+        addTilesMap();
+        addRoomsAndCorridors();
+        addInstance();
+    }  
+
+    void addTilesMap()
     {
         tiles = new TileType[MapColumns][];
         for (int i = 0; i < tiles.Length; i++)
         {
             tiles[i] = new TileType[MapRows];
         }
+    }
 
-        rooms = new Room[Random.Range(10,15)]; 
+    void addRoomsAndCorridors()
+    {
+        rooms = new Room[10]; 
         corridors = new Corridor[rooms.Length - 1];
+
         rooms[0] = new Room();
         corridors[0] = new Corridor();
         rooms[0].CreateRoom(roomWidth, roomHeight, MapColumns, MapRows);
@@ -44,8 +71,8 @@ public class RoomGenerator : MonoBehaviour
        
         for (int i = 1; i < rooms.Length; i++)
         {
-            roomWidth = Random.Range(6,10);
-            roomHeight = Random.Range(6,10);
+            roomWidth = Random.Range(4,6);
+            roomHeight = Random.Range(4,6);
             rooms[i] = new Room();
             rooms[i].CreateRoom(roomWidth, roomHeight, MapColumns, MapRows, corridors[i - 1]);
 
@@ -68,6 +95,8 @@ public class RoomGenerator : MonoBehaviour
                     tiles[xPos][yPos] = TileType.Floor;
                 }
             }
+            if (i == rooms.Length - 1) 
+                tiles[rooms[i].xPos + rooms[i].roomHeight/2][rooms[i].yPos + rooms[i].roomWidth/2] = TileType.End;
         }
 
         for (int i = 0; i < corridors.Length; i++) 
@@ -96,12 +125,15 @@ public class RoomGenerator : MonoBehaviour
                 tiles[xPos][yPos] = TileType.Floor;
             }
         }
+    }
 
+    void addInstance()
+    {
         for (int i = 0; i < tiles.Length; i++)
         {
             for (int j = 0; j < tiles[i].Length; j++)
             {
-                if (tiles[i][j] != TileType.Floor && CheckFloor(i,j))
+                if (tiles[i][j] != TileType.Floor && tiles[i][j] != TileType.End && CheckFloor(i,j))
                 {
                     Vector3 positionW = new Vector3(i, j, 0f);
                     GameObject wallInstance = Instantiate(wallTiles[0], positionW, Quaternion.identity) as GameObject;
@@ -114,14 +146,27 @@ public class RoomGenerator : MonoBehaviour
                     GameObject floorInstance = Instantiate(floorTiles[0], positionF, Quaternion.identity) as GameObject;
                     floorInstance.transform.parent = Map.transform;
                 } 
+                else if (tiles[i][j] == TileType.End)
+                {
+                    endPosition = new Vector3(i, j, 0f);
+                    GameObject endInstance = Instantiate(endTiles[0], endPosition, Quaternion.identity) as GameObject;
+                    endInstance.transform.parent = Map.transform;
+                }
             }
         }
-       
+    }
 
-    }  
-
-
-
+    void AddObjectsOnMap(GameObject[] tileArray, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            GameObject tileCreated = Instantiate(tileChoice, randomPosition, Quaternion.identity) as GameObject;
+            tileCreated.transform.parent = Map.transform;
+        }
+    }
 
     private bool CheckFloor(int x, int y)
     {
@@ -137,7 +182,7 @@ public class RoomGenerator : MonoBehaviour
             return false;
         }
 
-    } 
+    }
 
     Vector3 RandomPosition()
     {
@@ -146,16 +191,5 @@ public class RoomGenerator : MonoBehaviour
         gridPositions.RemoveAt(randomIndex);
         return randomPosition;
     }
-
-    void AddObjectsOnMap(GameObject[] tileArray, int minimum, int maximum)
-    {
-        int objectCount = Random.Range(minimum, maximum + 1);
-        for (int i = 0; i < objectCount; i++)
-        {
-            Vector3 randomPosition = RandomPosition();
-            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-            GameObject tileCreated = Instantiate(tileChoice, randomPosition, Quaternion.identity) as GameObject;
-            tileCreated.transform.parent = Map.transform;
-        }
-    }
+ 
 }
